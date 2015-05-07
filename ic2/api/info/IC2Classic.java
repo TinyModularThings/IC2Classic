d211561d8b897c4e30efc01397791331d07aa00b
Speiger <moritzstehr@gmx.de>
2015-05-07 15:11:39 +0200

    Thanks to xBony -Added a way to detect which IC2 is loaded, so you can now detect if My IC2 Classic is loaded or Immibis version is loaded or the Exp version is loaded or none is loaded or what the f..k is loaded xD Maybe should call this the Yolo api xD

--- ic2/api/info/IC2Classic.java
+++ ic2/api/info/IC2Classic.java
@@ -1,13 +1,38 @@
 package ic2.api.info;
 
+import cpw.mods.fml.common.Loader;
+
 public class IC2Classic
 {
-	public static Object ic2Classic;
 	public static IWindTicker windNetwork;
+	private static IC2Type ic2;
+	
+	
+	public static IC2Type getLoadedIC2Type()
+	{
+		if(ic2 == IC2Type.NeedLoad)
+		{
+			updateState();
+		}
+		return ic2;
+	}
+	
+	public static boolean isIc2ExpLoaded()
+	{
+		if(ic2 == IC2Type.NeedLoad)
+		{
+			updateState();
+		}
+		return ic2 == IC2Type.Experimental;
+	}
 	
 	public static boolean isIc2ClassicLoaded()
 	{
-		return ic2Classic != null;
+		if(ic2 == IC2Type.NeedLoad)
+		{
+			updateState();
+		}
+		return ic2 == IC2Type.SpeigersClassic;
 	}
 	
 	public static IWindTicker getWindNetwork()
@@ -18,6 +43,45 @@ public class IC2Classic
 	public static boolean enabledCustoWindNetwork()
 	{
 		return windNetwork != null;
+	}
+	
+	
+	
+	
+	static void updateState()
+	{
+		if(Loader.isModLoaded("IC2-Classic"))
+		{
+			ic2 = IC2Type.ImmibisClassic;
+			return;
+		}
+		if(Loader.isModLoaded("IC2"))
+		{
+			try
+			{
+				Class clz = Class.forName("ic2.core.IC2");
+				clz.getField("ironName").get(null);
+				clz.getField("displayNoUseItems").get(null);
+				ic2 = IC2Type.SpeigersClassic;
+			}
+			catch(Exception e)
+			{
+				ic2 = IC2Type.Experimental;
+			}
+		}
+		else
+		{
+			ic2 = IC2Type.None;
+		}
+	}
+	
+	public static enum IC2Type
+	{
+		NeedLoad,
+		Experimental,
+		SpeigersClassic,
+		ImmibisClassic,
+		None;
 	}
 	
 }
