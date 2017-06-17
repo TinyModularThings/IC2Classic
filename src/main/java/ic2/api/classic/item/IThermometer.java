38d640483aa3aeb2afb7cb147743289565a51eb9
Speiger <speiger@gmx.net>
2017-06-17 14:03:32 +0200

    Final API for 1.10.2 Release

---
+++ src/main/java/ic2/api/classic/item/IThermometer.java
@@ -0,0 +1,8 @@
+package ic2.api.classic.item;
+
+import net.minecraft.item.ItemStack;
+
+public interface IThermometer
+{
+	public boolean isThermometer(ItemStack stack);
+}
---
+++ src/main/java/ic2/api/classic/tile/IFakeMachine.java
@@ -0,0 +1,15 @@
+package ic2.api.classic.tile;
+
+/**
+ * 
+ * @author Speiger
+ * 
+ * Class to get upgrade information out of a Upgrade
+ * If you implement that class then it means you
+ * only want to read data for ReadOnly purpose.
+ * Not to use it on machines
+ */
+public interface IFakeMachine extends IMachine
+{
+	
+}
